6f0bd48ca38a54908ff295937858d8b3c78b7775
Yao <1415487814@qq.com>
2023-01-10 16:07:01 +0800

    引入几个包

--- service/src/main/java/com/lt/AuthApplication.java
+++ service/src/main/java/com/lt/AuthApplication.java
@@ -1,4 +1,4 @@
-package com.lt.AUTH;
+package com.lt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class AuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
 
 }
